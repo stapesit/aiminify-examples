41a7bed7fc995b23021a6f22019270ec4a3d590c
jorism1993 <jorismollinga@hotmail.com>
2025-06-03 16:24:44 +0200

    add compression

--- pytorch/yolo.py
+++ pytorch/yolo.py
@@ -1,42 +1,82 @@
-import copy
-
-from ultralytics import YOLO
 import torch
+from ultralytics import YOLO
+from ultralytics.engine.exporter import Exporter
+from onnxruntime.quantization import quantize_dynamic, QuantType
+
 
 from aiminify import minify
 from aiminify.minify_pytorch.utils import get_flops
 
-# Try any of the following
-# model = YOLO('yolov5s.pt').model.to('cpu')
-# model = YOLO('yolov8s.pt').model.to('cpu')
-model = YOLO('yolo11s.pt').model.to('cpu')
 
-rand_input = torch.randn(1, 3, 640, 640)
+def quantize_int8(model: torch.nn.Module) -> torch.nn.Module:
+    """
+    Applies static INT8 quantization to a YOLOv8 model.
 
-normal_flops = get_flops(model, rand_input)
-normal_params = sum(m.numel() for m in model.parameters())
+    Args:
+        model (torch.nn.Module): The YOLOv8 model to quantize.
 
-compressed_model, _ = minify(
-    model,
-    compression_strength=5,  # From 0 (no compression) to 5 (strongest)
+    Returns:
+        torch.nn.Module: The INT8 quantized model.
+    """
+    model.eval()
+    model.cpu()
 
-    # Everything below is optional
-    fine_tune=False,
-    quantization=False,
-    training_generator=None,
-    validation_generator=None,
-    loss_function=None,
-    optimizer=None,
-    precision='mixed',
-    accumulation_steps=1,
-    debug_mode=False,
-)
+    custom = {
+        "imgsz": model.args["imgsz"],
+        "batch": 1,
+        "data": None,
+        "device": None,  # reset to avoid multi-GPU errors
+        "verbose": False,
+        "format": "onnx",
+        "simplify": True,
+        "opset": 13,
+        "int8": True,
+        "mode": "export",
+    }
 
-compressed_flops = get_flops(compressed_model, rand_input)
-compressed_params = sum(m.numel() for m in compressed_model.parameters())
+    onnx_model = Exporter(overrides=custom, )(model=model)
 
-print(f'Normal flops: {normal_flops:,}')
-print(f'Compressed flops: {compressed_flops:,}')
+    quantize_dynamic(
+        model_input=onnx_model,
+        model_output='model_int8.onnx',
+        weight_type=QuantType.QInt8  # or QuantType.QUInt8
+    )
 
-print(f'Normal params: {normal_params:,}')
-print(f'Compressed params: {compressed_params:,}')
+
+if __name__ == "__main__":
+    # Try any of the following
+    model = YOLO('yolov8s-seg.pt').model.to('cpu')
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5s').to('cpu')
+    # model = YOLO('yolov5s.pt').model.to('cpu')
+
+    rand_input = torch.randn(1, 3, 640, 640)
+
+    normal_flops = get_flops(model, rand_input)
+    normal_params = sum(m.numel() for m in model.parameters())
+
+    compressed_model, _ = minify(
+        model,
+        compression_strength=5,  # From 0 (no compression) to 5 (strongest)
+
+        # Everything below is optional
+        fine_tune=False,
+        quantization=False,
+        training_generator=None,
+        validation_generator=None,
+        loss_function=None,
+        optimizer=None,
+        precision='mixed',
+        accumulation_steps=1,
+        debug_mode=False,
+    )
+
+    compressed_flops = get_flops(compressed_model, rand_input)
+    compressed_params = sum(m.numel() for m in compressed_model.parameters())
+
+    print(f'Normal flops: {normal_flops:,}')
+    print(f'Compressed flops: {compressed_flops:,}')
+
+    print(f'Normal params: {normal_params:,}')
+    print(f'Compressed params: {compressed_params:,}')
+
+    int8_model = quantize_int8(compressed_model)
